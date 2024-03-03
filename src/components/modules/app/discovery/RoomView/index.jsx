aadcfeea81edabc49172d5365c1aae9a78a3d519
sawsew467 <bao.thang.1912@gmail.com>
2024-03-03 13:25:23 +0700

    feat: ui

--- src/components/modules/app/discovery/RoomView/index.jsx
+++ src/components/modules/app/discovery/RoomView/index.jsx
@@ -1,5 +1,52 @@
+import { Flex } from "antd";
+
 function RoomView() {
-  return <div>RoomView</div>;
+  return (
+    <Flex
+      vertical
+      flex={1}
+      style={{
+        height: "100%",
+      }}
+      gap={12}
+    >
+      <Flex
+        style={{
+          padding: "16px",
+          background: "#fff",
+          borderRadius: 12,
+        }}
+        justify="space-between"
+      >
+        <Flex>
+          <p
+            style={{
+              fontSize: 16,
+              fontWeight: 500,
+            }}
+          >
+            Vo Thi Thuy Suong
+          </p>
+        </Flex>
+        <p
+          style={{
+            fontSize: 16,
+            fontWeight: 500,
+            color: "#ff4848",
+          }}
+        >
+          00:09:32
+        </p>
+      </Flex>
+      <div
+        style={{
+          flex: 1,
+          background: "#fff",
+          borderRadius: 12,
+        }}
+      ></div>
+    </Flex>
+  );
 }
 
 export default RoomView;
