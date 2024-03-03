4d2c996f9f8bccbbb2f72f142ea26e2b291b0fbc
sawsew467 <bao.thang.1912@gmail.com>
2024-03-03 13:05:57 +0700

    feat: matched view ui

--- src/components/modules/app/discovery/MatchedView/index.jsx
+++ src/components/modules/app/discovery/MatchedView/index.jsx
@@ -1,5 +1,76 @@
+"use client";
+
+import { useState } from "react";
+import { Flex } from "antd";
+import { HeartFilled } from "@ant-design/icons";
+import { useRouter } from "next/navigation";
+
+import Card from "./Card";
+import Button from "@/components/core/common/Button";
+
+import * as S from "./MatchedView.styles";
+
 function MatchedView() {
-  return <div>MatchedView</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const handleReady = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      const roomId = "room1234";
+      router.push(`?roomId=${roomId}`);
+    }, 5000);
+  };
+
+  return (
+    <>
+      <Flex
+        justify="center"
+        align="center"
+        flex={1}
+        gap={40}
+        style={{
+          height: "100%",
+        }}
+        vertical
+      >
+        <Flex align="center" gap={20}>
+          <Card
+            info={{
+              name: "Bao Thang",
+              age: 21,
+              avatar: "/images/common/person 2.jpg",
+            }}
+          ></Card>
+          <S.HeartWrap>
+            <S.HeartButton justify="center" align="center">
+              <HeartFilled />
+            </S.HeartButton>
+          </S.HeartWrap>
+          <Card
+            info={{
+              name: "Thuy Suong",
+              age: 20,
+              avatar: "/images/common/person 1.jpg",
+            }}
+          ></Card>
+        </Flex>
+        <Flex vertical align="center" gap={8}>
+          <p
+            style={{
+              fontWeight: "bold",
+              fontSize: 20,
+            }}
+          >
+            Đã kết nối đến đối phương thành công!
+          </p>
+          <Button type="primary" onClick={handleReady} loading={isLoading}>
+            BẮT ĐẦU
+          </Button>
+        </Flex>
+      </Flex>
+    </>
+  );
 }
 
 export default MatchedView;
