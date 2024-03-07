b081e22edc37f95a3cd4a5f7b39f6554213d7d06
sawsew467 <bao.thang.1912@gmail.com>
2024-03-07 23:39:33 +0700

    feat: admin layout

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -1,5 +1,10 @@
 import AdminLayout from "@/components/core/layouts/AdminLayout";
 
+export const metadata = {
+  title: "Trang quản trị",
+  description: "...",
+};
+
 export default function RootAdminLayout({ children }) {
   return (
     <>
